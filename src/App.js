28953a84ff9d5e81d9996513fbad12ee8bcca604
RezwanIUS <is.rezwan@gmail.com>
2021-03-10 23:11:00 +0600

    Navigation Add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import MainComponent from './component/MainComponent';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      
+        <MainComponent />
     </div>
   );
 }
---
+++ src/component/MainComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header/Header';
+import Body from './Body/Body';
+import Footer from './Footer/Footer.js';
+
+const MainComponent = () =>{
+
+    return(
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )
+}
+
+export default MainComponent;
